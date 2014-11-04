22445d5f667f9da4ae8164b01156d8350c4a6db9
Stefan Schälle <schaelle@gmail.com>
2014-11-04 16:49:01 +0100

    Missing file

---
+++ TerrificNet.ViewEngine/Config/ITerrificNetConfig.cs
@@ -0,0 +1,10 @@
+﻿namespace TerrificNet.ViewEngine.Config
+{
+	public interface ITerrificNetConfig
+	{
+		string BasePath { get; }
+		string ViewPath { get; }
+		string AssetPath { get; }
+		string DataPath { get; }
+	}
+}
