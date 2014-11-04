9de5f8b35ccc3e8dd770374c35653e3527de5931
Marco Schälle <marco.schaelle@namics.com>
2014-11-04 14:45:24 +0100

    merge

---
+++ TerrificNet.ViewEngine/DisplayHelpers.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using Nustache.Core;
+
+namespace TerrificNet.ViewEngine
+{
+	public class DisplayHelpers
+	{
+		public static void Register()
+		{
+			if (!Helpers.Contains("module"))
+				Helpers.Register("module", TerrificModuleHelper);
+		}
+
+		internal static void TerrificModuleHelper(RenderContext ctx, IList<object> args, IDictionary<string, object> options,
+			RenderBlock fn, RenderBlock inverse)
+		{
+			object template;
+			if (!options.TryGetValue("template", out template))
+				return;
+
+			var templateName = template as string;
+			if (string.IsNullOrEmpty(templateName))
+				return;
+
+			ctx.Write("start("+templateName+")");
+			ctx.Include(templateName, "");
+		}
+	}
+}
