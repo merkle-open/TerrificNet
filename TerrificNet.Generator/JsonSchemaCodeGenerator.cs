58f8973a3cb4b658a4172cb2f0cef7a31d7cbbc3
Marco Schälle <marco.schaelle@namics.com>
2014-11-05 15:41:46 +0100

    simple code compilation

--- TerrificNet.Generator/JsonSchemaCodeGenerator.cs
+++ TerrificNet.Generator/JsonSchemaCodeGenerator.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Reflection;
-using System.Reflection.Emit;
 using System.Text.RegularExpressions;
 using Newtonsoft.Json.Schema;
 using Roslyn.Compilers;
@@ -11,22 +10,26 @@ using Roslyn.Compilers.CSharp;
 
 namespace TerrificNet.Generator
 {
-    public class JsonSchemaCodeGenerator : IJsonSchemaCodeGenerator
-    {
-        public string Generate(JsonSchema schema)
-        {
-            var root = GetSyntax(schema);
-            return root.ToFullString();
-        }
+	public class JsonSchemaCodeGenerator : IJsonSchemaCodeGenerator
+	{
+		public string Generate(JsonSchema schema)
+		{
+		    var root = GetSyntax(schema);
+		    return root.NormalizeWhitespace().ToFullString();
+		}
 
-        private static CompilationUnitSyntax GetSyntax(JsonSchema schema)
-        {
-            var root = Syntax.CompilationUnit()
-                .WithMembers(Syntax.List(GenerateClass(schema)));
-            return root;
-        }
+	    private static CompilationUnitSyntax GetSyntax(JsonSchema schema)
+	    {
+	        var typeContext = new Dictionary<string, MemberDeclarationSyntax>();
 
-        public Type Compile(JsonSchema schema)
+	        RoslynExtension.GenerateClass(schema, typeContext, string.Empty);
+
+	        var root = Syntax.CompilationUnit()
+	            .WithMembers(Syntax.List(typeContext.Values.ToArray()));
+	        return root;
+	    }
+
+	    public Type Compile(JsonSchema schema)
         {
             var output = GetSyntax(schema);
 
@@ -55,103 +58,112 @@ namespace TerrificNet.Generator
                     return assembly.GetTypes().First();
                 }
             }
-            
-            return null;
-        }
-
-        private static MemberDeclarationSyntax GenerateClass(JsonSchema schema)
-        {
-            if (schema.Type == JsonSchemaType.Object)
-            {
-                var className = RoslynExtension.NormalizeClassName(schema.Title);
-
-                if (string.IsNullOrEmpty(className))
-                    throw new Exception("Title not set");
-
-                return Syntax.ClassDeclaration(
-                    Syntax.Identifier(className))
-                    .WithKeyword(
-                        Syntax.Token(SyntaxKind.ClassKeyword, Syntax.TriviaList(Syntax.Space)))
-                    .WithOpenBraceToken(
-                        Syntax.Token(SyntaxKind.OpenBraceToken))
-                    .WithMembers(new SyntaxList<MemberDeclarationSyntax>().AddProperties(schema))
-                    .WithCloseBraceToken(
-                        Syntax.Token(SyntaxKind.CloseBraceToken));
-            }
 
             return null;
         }
+	}
 
+	static class RoslynExtension
+	{
+		public static SyntaxList<MemberDeclarationSyntax> AddProperties(this SyntaxList<MemberDeclarationSyntax> memberList, JsonSchema schema, Dictionary<string, MemberDeclarationSyntax> typeContext)
+		{
+			var result = memberList;
+			foreach (var property in schema.Properties)
+			{
+				var propertyName = NormalizeClassName(property.Key);
 
-    }
+				result = result.Add(Syntax.PropertyDeclaration(GetPropertyType(property.Value, typeContext, propertyName).WithTrailingTrivia(Syntax.Space), propertyName)
+					.WithAccessorList(Syntax.AccessorList(Syntax.List(
+						Syntax.AccessorDeclaration(SyntaxKind.GetAccessorDeclaration)
+					.WithSemicolonToken(Syntax.Token(SyntaxKind.SemicolonToken)),
+						Syntax.AccessorDeclaration(SyntaxKind.SetAccessorDeclaration)
+					.WithSemicolonToken(Syntax.Token(SyntaxKind.SemicolonToken))
+					))));
+			}
 
-    static class RoslynExtension
-    {
-        public static SyntaxList<MemberDeclarationSyntax> AddProperties(this SyntaxList<MemberDeclarationSyntax> memberList, JsonSchema schema)
-        {
-            var result = memberList;
-            foreach (var property in schema.Properties)
-            {
-                var propertyName = NormalizeClassName(property.Key);
+			return result;
+		}
 
-                result = result.Add(Syntax.PropertyDeclaration(GetPropertyType(property.Value).WithTrailingTrivia(Syntax.Space), propertyName)
-                    .WithAccessorList(Syntax.AccessorList(Syntax.List(
-                        Syntax.AccessorDeclaration(SyntaxKind.GetAccessorDeclaration)
-                    .WithSemicolonToken(Syntax.Token(SyntaxKind.SemicolonToken)),
-                        Syntax.AccessorDeclaration(SyntaxKind.SetAccessorDeclaration)
-                    .WithSemicolonToken(Syntax.Token(SyntaxKind.SemicolonToken))
-                    ))));
-            }
+		private static TypeSyntax GetPropertyType(JsonSchema value, Dictionary<string, MemberDeclarationSyntax> typeContext, string propertyName)
+		{
+			switch (value.Type)
+			{
+				case JsonSchemaType.String:
+					return Syntax.ParseTypeName("string");
+				case JsonSchemaType.Integer:
+					return Syntax.ParseTypeName("int");
+				case JsonSchemaType.Float:
+					return Syntax.ParseTypeName("double");
+				case JsonSchemaType.Boolean:
+					return Syntax.ParseTypeName("bool");
+				case JsonSchemaType.Array:
+					return Syntax.IdentifierName(string.Format("List<{0}>", NormalizeClassName(value.Title)));
+				case JsonSchemaType.Object:
+					GenerateClass(value, typeContext, propertyName);
+					return Syntax.IdentifierName(NormalizeClassName(propertyName));
+				default:
+					return Syntax.ParseTypeName("object");
+			}
+		}
 
-            return result;
-        }
+		public static void GenerateClass(JsonSchema schema, Dictionary<string, MemberDeclarationSyntax> typeContext, string propertyName)
+		{
+			if (schema.Type == JsonSchemaType.Object)
+			{
+				var className = NormalizeClassName(schema.Title);
+				if (string.IsNullOrEmpty(className))
+					className = propertyName;
 
-        private static TypeSyntax GetPropertyType(JsonSchema value)
-        {
-            switch (value.Type)
-            {
-                case JsonSchemaType.String:
-                    return Syntax.ParseTypeName("string");
-                case JsonSchemaType.Integer:
-                    return Syntax.ParseTypeName("int");
-                case JsonSchemaType.Float:
-                    return Syntax.ParseTypeName("double");
-                case JsonSchemaType.Boolean:
-                    return Syntax.ParseTypeName("bool");
-                case JsonSchemaType.Array:
-                    return Syntax.ParseTypeName("List");
-                default:
-                    return Syntax.ParseTypeName("object");
-            }
-        }
+				if (typeContext.ContainsKey(className))
+					return;
 
-        public static string NormalizeClassName(string input)
-        {
-            return ConvertToPascalCase(input).Replace(" ", "").Trim();
-        }
+				if (string.IsNullOrEmpty(className))
+					throw new Exception("Title not set");
 
-        private static string ConvertToPascalCase(string input)
-        {
-            return new Regex(@"\p{Lu}\p{Ll}+|\p{Lu}+(?!\p{Ll})|\p{Ll}+|\d+").Replace(input, EvaluatePascal);
-        }
+				var classDeclaration= Syntax.ClassDeclaration(
+					Syntax.Identifier(className))
+					.WithKeyword(
+						Syntax.Token(SyntaxKind.ClassKeyword, Syntax.TriviaList(Syntax.Space)))
+					.WithOpenBraceToken(
+						Syntax.Token(SyntaxKind.OpenBraceToken))
+					.WithMembers(new SyntaxList<MemberDeclarationSyntax>().AddProperties(schema, typeContext))
+					.WithCloseBraceToken(
+						Syntax.Token(SyntaxKind.CloseBraceToken));
 
-        private static string EvaluatePascal(Match match)
-        {
-            var value = match.Value;
-            var valueLength = value.Length;
+				typeContext.Add(className, classDeclaration);
+			}
+		}
 
-            if (valueLength == 1)
-                return value.ToUpper();
+		public static string NormalizeClassName(string input)
+		{
+			if (string.IsNullOrEmpty(input))
+				return input;
 
-            if (valueLength <= 2 && IsWordUpper(value))
-                return value;
+			return ConvertToPascalCase(input).Replace(" ", "").Trim();
+		}
 
-            return value.Substring(0, 1).ToUpper() + value.Substring(1, valueLength - 1).ToLower();
-        }
+		private static string ConvertToPascalCase(string input)
+		{
+			return new Regex(@"\p{Lu}\p{Ll}+|\p{Lu}+(?!\p{Ll})|\p{Ll}+|\d+").Replace(input, EvaluatePascal);
+		}
 
-        private static bool IsWordUpper(string word)
-        {
-            return word.All(c => !Char.IsLower(c));
-        }
-    }
+		private static string EvaluatePascal(Match match)
+		{
+			var value = match.Value;
+			var valueLength = value.Length;
+
+			if (valueLength == 1)
+				return value.ToUpper();
+
+			if (valueLength <= 2 && IsWordUpper(value))
+				return value;
+
+			return value.Substring(0, 1).ToUpper() + value.Substring(1, valueLength - 1).ToLower();
+		}
+
+		private static bool IsWordUpper(string word)
+		{
+			return word.All(c => !Char.IsLower(c));
+		}
+	}
 }
