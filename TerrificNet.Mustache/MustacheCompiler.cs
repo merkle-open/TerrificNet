be4a3591330e8c62fec175284ea3b4463b7c523e
Marco Schälle <marco.schaelle@namics.com>
2014-11-05 16:47:58 +0100

    mustache parser

---
+++ TerrificNet.Mustache/MustacheCompiler.cs
@@ -0,0 +1,136 @@
+﻿
+using System.Collections.Generic;
+
+namespace TerrificNet.Mustache
+{
+    public class MustacheCompiler
+    {
+        public MustacheCompiler()
+        {
+        }
+
+        public void Compile()
+        {
+            var scanner = new Scanner();
+            var parser = new Parser(scanner);
+
+            parser.Parse("");
+        }
+    }
+
+    public class Parser
+    {
+        private readonly Scanner _scanner;
+
+        public Parser(Scanner scanner)
+        {
+            _scanner = scanner;
+        }
+
+        public MustacheNode Parse(string content)
+        {
+            MustacheBlock currentTemplate = new MustacheTemplate();
+            var stack = new Stack<MustacheBlock>();
+            stack.Push(currentTemplate);
+            foreach (var literal in _scanner.Scan(content))
+            {
+                currentTemplate = stack.Peek();
+
+                if (literal.Type == Scanner.LiteralType.Text)
+                    currentTemplate.Children.Add(new MustacheLiteralNode(literal.Content));
+                else if (literal.Type == Scanner.LiteralType.VariableReference)
+                    currentTemplate.Children.Add(new MustacheStatementNode(literal.Content));
+                else if (literal.Type == Scanner.LiteralType.BlockStart)
+                {
+                    var block = new MustacheBlock(literal.Content);
+                    currentTemplate.Children.Add(block);
+                    stack.Push(block);
+                }
+                else if (literal.Type == Scanner.LiteralType.EndSection)
+                {
+                    stack.Pop();
+                }
+            }
+
+            return currentTemplate;
+        }
+    }
+
+    public interface IMustacheNodeVisitor
+    {
+        void VisitBlockBegin(MustacheBlock block);
+        void VisitBlockEnd(MustacheBlock block);
+        void VisitStatement(MustacheStatementNode statement);
+        void VisitLiteral(MustacheLiteralNode literal);
+    }
+
+    public abstract class MustacheNode
+    {
+        public abstract void Accept(IMustacheNodeVisitor visitor);
+    }
+
+    public class MustacheTemplate : MustacheBlock
+    {
+    }
+
+    public class MustacheBlock : MustacheNode
+    {
+        public string Statement { get; set; }
+        public IList<MustacheNode> Children { get; set; }
+
+        public MustacheBlock(string statement = null)
+        {
+            Statement = statement;
+            Children = new List<MustacheNode>();
+        }
+
+        public override void Accept(IMustacheNodeVisitor visitor)
+        {
+            visitor.VisitBlockBegin(this);
+            foreach (var child in Children)
+                child.Accept(visitor);
+
+            visitor.VisitBlockEnd(this);
+        }
+    }
+
+    public class MustacheStatementNode : MustacheNode
+    {
+        public string Statement { get; set; }
+
+        public MustacheStatementNode(string statement)
+        {
+            Statement = statement;
+        }
+
+        public override void Accept(IMustacheNodeVisitor visitor)
+        {
+            visitor.VisitStatement(this);
+        }
+    }
+
+    public class MustacheVariableReference
+    {
+        public string[] Parts { get; set; }
+
+        public MustacheVariableReference(string[] parts)
+        {
+            Parts = parts;
+        }
+    }
+
+    public class MustacheLiteralNode : MustacheNode
+    {
+        public string Text { get; set; }
+
+        public MustacheLiteralNode(string text)
+        {
+            Text = text;
+        }
+
+        public override void Accept(IMustacheNodeVisitor visitor)
+        {
+            visitor.VisitLiteral(this);
+        }
+    }
+}
---
+++ TerrificNet.Mustache/Scanner.cs
@@ -0,0 +1,164 @@
+﻿using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace TerrificNet.Mustache
+{
+    public class Scanner
+    {
+        private static readonly Regex DelimitersRegex = new Regex(@"^=\s*(\S+)\s+(\S+)\s*=$");
+
+        public class Literal
+        {
+            public LiteralType Type { get; set; }
+            public string Content { get; set; }
+            public int Line { get; set; }
+            public int Column { get; set; }
+
+            public Literal(LiteralType type, string content, int line = 0, int column = 0)
+            {
+                Type = type;
+                Content = content;
+                Line = line;
+                Column = column;
+            }
+        }
+
+        public enum LiteralType
+        {
+            Text,
+            BlockStart,
+            InvertBlockStart,
+            TemplateDefinition,
+            EndSection,
+            TemplateInclude,
+            VariableReference
+        }
+
+        public IEnumerable<Literal> Scan(string template)
+        {
+            var regex = MakeRegex("{{", "}}");
+            var i = 0;
+            var lineEnded = false;
+
+            while (true)
+            {
+                Match m;
+
+                if ((m = regex.Match(template, i)).Success)
+                {
+                    var previousLiteral = template.Substring(i, m.Index - i);
+
+                    var leadingWhiteSpace = m.Groups[1];
+                    var leadingLineEnd = m.Groups[2];
+                    var leadingWhiteSpaceOnly = m.Groups[3];
+                    var marker = m.Groups[4].Value.Trim();
+                    var trailingWhiteSpace = m.Groups[5];
+                    var trailingLineEnd = m.Groups[6];
+
+                    var isStandalone = (leadingLineEnd.Success || (lineEnded && m.Index == i)) && trailingLineEnd.Success;
+
+                    Literal part = null;
+
+                    if (marker[0] == '=')
+                    {
+                        var delimiters = DelimitersRegex.Match(marker);
+
+                        if (delimiters.Success)
+                        {
+                            var start = delimiters.Groups[1].Value;
+                            var end = delimiters.Groups[2].Value;
+                            regex = MakeRegex(start, end);
+                        }
+                    }
+                    else if (marker[0] == '#')
+                    {
+                        part = new Literal(LiteralType.BlockStart, marker.Substring(1).Trim(), m.Index);
+                    }
+                    else if (marker[0] == '^')
+                    {
+                        part = new Literal(LiteralType.InvertBlockStart, marker.Substring(1).Trim(), m.Index);
+                    }
+                    else if (marker[0] == '<')
+                    {
+                        part = new Literal(LiteralType.TemplateDefinition, marker.Substring(1).Trim(), m.Index);
+                    }
+                    else if (marker[0] == '/')
+                    {
+                        part = new Literal(LiteralType.EndSection, marker.Substring(1).Trim(), m.Index);
+                    }
+                    else if (marker[0] == '>')
+                    {
+                        part = new Literal(LiteralType.TemplateInclude, marker.Substring(1).Trim(), m.Index/*, lineEnded || i == 0 ? leadingWhiteSpaceOnly.Value : null*/);
+                    }
+                    else if (marker[0] != '!')
+                    {
+                        if (marker == "else")
+                        {
+                            part = new Literal(LiteralType.BlockStart, marker, m.Index);
+                        }
+                        else
+                        {
+                            part = new Literal(LiteralType.VariableReference, marker, m.Index);
+                            isStandalone = false;
+                        }
+                    }
+
+                    if (!isStandalone)
+                    {
+                        previousLiteral += leadingWhiteSpace;
+                    }
+                    else
+                    {
+                        previousLiteral += leadingLineEnd;
+
+                        if (part.Type == LiteralType.TemplateInclude)
+                        {
+                            previousLiteral += leadingWhiteSpaceOnly;
+                        }
+                    }
+
+                    if (previousLiteral != "")
+                    {
+                        yield return new Literal(LiteralType.Text, previousLiteral, m.Index);
+                    }
+
+                    if (part != null)
+                    {
+                        yield return part;
+                    }
+
+                    i = m.Index + m.Length;
+
+                    if (!isStandalone)
+                    {
+                        i -= trailingWhiteSpace.Length;
+                    }
+
+                    lineEnded = trailingLineEnd.Success;
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            if (i < template.Length)
+            {
+                var remainingLiteral = template.Substring(i);
+
+                yield return new Literal(LiteralType.Text, remainingLiteral);
+            }
+        }
+
+        private static Regex MakeRegex(string start, string end)
+        {
+            return new Regex(
+                @"((^|\r?\n)?([\r\t\v ]*))" +
+                Regex.Escape(start) +
+                @"([\{]?[^" + Regex.Escape(end.Substring(0, 1)) + @"]+?\}?)" +
+                Regex.Escape(end) +
+                @"([\r\t\v ]*(\r?\n|$)?)"
+            );
+        }
+    }
+}
