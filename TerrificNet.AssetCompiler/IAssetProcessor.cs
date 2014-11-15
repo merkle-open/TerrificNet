33161026bfe94950364c7946f3a944829aafae1a
Christoph Buehler <christoph.buehler@namics.com>
2014-11-15 12:39:12 +0100

    adding interface for processor

---
+++ TerrificNet.AssetCompiler/IAssetProcessor.cs
@@ -0,0 +1,14 @@
+﻿
+using System.Threading.Tasks;
+using TerrificNet.AssetCompiler.Configuration;
+
+namespace TerrificNet.AssetCompiler
+{
+    public interface IAssetProcessor
+    {
+        bool Minify { get; set; }
+        Task ProcessAsync();
+        Task ProcessAsync(string configPath);
+        Task ProcessAsync(TerrificConfig config);
+    }
+}
--- TerrificNet.AssetCompiler/Processors/BuildAssetProcessor.cs
+++ TerrificNet.AssetCompiler/Processors/BuildAssetProcessor.cs
@@ -1,7 +1,28 @@
-﻿namespace TerrificNet.AssetCompiler.Processors
-{
-    public class BuildAssetProcessor
-    {
+﻿using System.Threading.Tasks;
+using TerrificNet.AssetCompiler.Configuration;
 
+namespace TerrificNet.AssetCompiler.Processors
+{
+    public class BuildAssetProcessor : IAssetProcessor
+    {
+        #region Implementation of IAssetProcessor
+
+        public bool Minify { get; set; }
+        public Task ProcessAsync()
+        {
+            return ProcessAsync(TerrificConfig.Parse());
+        }
+
+        public Task ProcessAsync(string configPath)
+        {
+            return ProcessAsync(TerrificConfig.Parse(configPath));
+        }
+
+        public Task ProcessAsync(TerrificConfig config)
+        {
+            return null;
+        }
+
+        #endregion
     }
 }
