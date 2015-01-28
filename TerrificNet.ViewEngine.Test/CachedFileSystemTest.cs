818ca5f89c3abfb6760cb9d692ea7ba8115c2f23
Stefan Schälle <stefan.schaelle@namics.com>
2015-01-28 18:09:58 +0100

    Test Project added

---
+++ TerrificNet.ViewEngine.Test/CachedFileSystemTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TerrificNet.ViewEngine.Test
+{
+	[TestClass]
+	public class CachedFileSystemTest
+	{
+		private IFileSystem _fileSystem;
+
+		[TestInitialize]
+		public void Init()
+		{
+			// Cleanup previous tests
+			foreach (var file in Directory.GetFiles("./", "*"))
+			{
+				Console.WriteLine(file);
+			}
+
+			_fileSystem = new CachedFileSystem();
+		}
+
+		[TestMethod]
+		public void TestRead()
+		{
+		}
+	}
+}
