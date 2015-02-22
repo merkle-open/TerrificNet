89f1712dcea9f820efdc76808b9867a0fd96f02b
Marco Schaelle <marco.schaelle@namics.com>
2015-02-22 10:51:56 +0100

    extracted controller to own file

--- TerrificNet/Core/js/ViewEngine.ts
+++ TerrificNet/Core/js/ViewEngine.ts
@@ -49,45 +49,6 @@ module Tcn {
         }
     }
 
-	export class ControllerActivator {
-		activate(name: string) : Object {
-			var ctor = eval(name);
-			return new ctor();
-		}
-	}
-
-	export class ControllerInjector {
-		constructor(private activator: ControllerActivator) {
-		}
-
-		public lookup(element:JQuery): void {
-			element.find("[data-tc-controller]").each((idx, elem) => this.register(elem));
-			element.find("[data-tc-action]").each((idx, elem) => this.registerAction(elem));
-		}
-
-		private registerAction(element: Element) {
-			var $elem = $(element);
-			var action: string = $elem.data("tc-action");
-			$elem.click(() => {
-				var $container = $elem.parent("[data-tc-controller]");
-				var controller = $container.data("tc-controller-inst");
-				var result = controller[action]();
-
-				ViewEngine.loadAndRenderAsync($container.data("tc-templateid"), result).then(html => {
-					$container.html(html);
-					this.lookup($container);
-				});
-			});
-		}
-
-		private register(element: Element) {
-			var name:string = $(element).data("tc-controller");
-			var controller = this.activator.activate(name);
-
-			$(element).data("tc-controller-inst", controller);
-		}
-	}
-
 	export var TemplateRepository: ITemplateRepository = new TemplateRepositoryInternal("/js/{id}?jsRepository=Tcn.TemplateRepository");
 	export var ViewEngine: ViewEngineImplementation = new ViewEngineImplementation(TemplateRepository);
 
