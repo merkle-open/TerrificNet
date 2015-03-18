7e5df057f68c854e4f5a66ce1bfbe40888ed690d
Christoph Buehler <christoph.buehler@namics.com>
2015-03-18 18:02:10 +0100

    adding basic drag n grop

--- TerrificNet/Web/assets/pageEditor.js
+++ TerrificNet/Web/assets/pageEditor.js
@@ -1,32 +1,86 @@
-﻿$(document).ready(function() {
+﻿$(document).ready(function () {
+    //prototype extensions
+    if (!Array.prototype.get) {
+        Array.prototype.get = function (predicate) {
+            var found = null;
+            this.forEach(function (e) {
+                if (!found && predicate(e)) {
+                    found = e;
+                }
+            });
+            return found;
+        };
+    }
+
+    if (!Array.prototype.forEach) {
+        Array.prototype.forEach = function (func) {
+            for (var x = 0; x < this.length; x++)
+                func(this[x], x);
+        };
+    }
+
     //class definitions
-    function SiteDom(json){
+    function JsonDom(json) {
         var domObject = JSON.parse(json);
 
         console.log(domObject);
     }
 
+    function Module($el) {
+        this.id = $el.data('id');
+        this.html = $el.find('.js-module-definition').html();
+    }
+
 
     //variables
     var $editor = $('.page-editor'),
-        dom = new SiteDom($('#siteDefinition', $editor).html());
+        dom = new JsonDom($('#siteDefinition', $editor).html()),
+        modules = [];
 
 
     //functions
 
 
     //event binders
-    $('.sidebar .module', $editor).on('dragstart', function(e){
-        console.log(e);
+    $('.sidebar .module', $editor).on('dragstart', function (e) {
+        e.originalEvent.dataTransfer.setData('id', $(this).data('id'));
+
     });
 
-    $('.plh.module .btn-delete', $editor).click(function(){
-        var $this = $(this);
+    $editor
+        .on('dragover', '.plh', function (e) {
+            e.preventDefault();
+            $(this).addClass('drag-over');
+        })
+        .on('dragleave', '.plh', function () {
+            $(this).removeClass('drag-over');
+        })
+        .on('drop', '.plh', function (e) {
+            console.log(e.originalEvent.dataTransfer.getData('id'))
+        });
 
-        console.log($this)
+    $('.plh.module .btn-delete', $editor).click(function () {
+        var $this = $(this),
+            $plh = $this.parent().prevAll('.plh:not(.module)'),
+            plhId = /plh_(.*)_start/g.exec($plh.attr('id'))[1];
+
+        var $modStart = $this.parent();
+
+        if (!$modStart.nextAll('.plh.module').length) {
+            throw new Error("no module end found.");
+        }
+        var $modEnd = $modStart.nextAll('.plh.module').first();
+        var $between = $modStart.nextUntil($modEnd);
+
+        //todo: remove from json dom
+
+        $this.tooltip('hide');
+        [$modStart, $between, $modEnd].forEach(function (e) {
+            e.remove();
+        });
     });
 
-    $editor.on('click', 'a', function(){
+    $editor.on('click', 'a', function () {
         //prevent links from firing
         return false;
     });
@@ -34,5 +88,10 @@
     //initialize tooltips
     $('[data-toggle="tooltip"]', $editor).tooltip({
         container: '.page-editor'
+    });
+
+    //init.
+    $('.sidebar .module', $editor).each(function (k, v) {
+        modules.push(new Module($(v)));
     });
 });
