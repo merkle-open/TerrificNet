f393d9e57fa6e0e65204946596c7180e04b3e3cf
Stefan Schälle <schaelle@gmail.com>
2015-03-25 17:43:06 +0100

    Cleanup

---
+++ TerrificNet/Models/PageEditor/PageEditModuleModel.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace TerrificNet.Models.PageEditor
+{
+	public class PageEditModuleModel
+	{
+		[JsonProperty("id")]
+		public string Id { get; set; }
+
+		[JsonProperty("name")]
+		public string Name { get; set; }
+
+		[JsonProperty("skins")]
+		public IEnumerable<string> Skins { get; set; }
+
+		[JsonProperty("variations")]
+		public string Variations { get; set; }
+	}
+}
