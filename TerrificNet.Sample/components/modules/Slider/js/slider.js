b080cc538657d56b5cec94e96b5867a99bb24685
Denis Skeledzic-Gemperli <denis.skeledzic@namics.com>
2015-04-20 13:12:01 +0200

    update color in button add new module (slider) update Tab module

---
+++ TerrificNet.Sample/components/modules/Slider/js/slider.js
@@ -0,0 +1,37 @@
+(function($) {
+	"use strict";
+	/**
+	 * Slider module implementation.
+	 *
+	 * @author Denis Skeledzic-Gemperli <denis.skeledzic@namics.com>
+	 * @namespace Tc.Module
+	 * @class Slider
+	 * @extends Tc.Module
+	 */
+	Tc.Module.Slider = Tc.Module.extend({
+
+		init: function($ctx, sandbox, modId) {
+			this._super($ctx, sandbox, modId);
+
+
+
+		},
+
+		on: function(callback) {
+			var mod = this,
+				$ctx = mod.$ctx;
+
+
+
+			callback();
+		},
+
+		after: function() {
+			var mod = this,
+				$ctx = mod.$ctx;
+
+
+		}
+
+	});
+}(Tc.$));
