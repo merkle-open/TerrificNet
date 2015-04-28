3028211f1c5dac0f180e3bc955dba9cec670dbca
Denis Skeledzic-Gemperli <denis.skeledzic@namics.com>
2015-04-28 09:01:06 +0200

    update slider add title & text add new module

---
+++ TerrificNet.Sample/components/modules/DownloadList/js/downloadlist.js
@@ -0,0 +1,37 @@
+(function($) {
+	"use strict";
+	/**
+	 * DownloadList module implementation.
+	 *
+	 * @author Denis Skeledzic-Gemperli <denis.skeledzic@namics.com>
+	 * @namespace Tc.Module
+	 * @class DownloadList
+	 * @extends Tc.Module
+	 */
+	Tc.Module.DownloadList = Tc.Module.extend({
+
+		init: function($ctx, sandbox, modId) {
+			this._super($ctx, sandbox, modId);
+
+
+
+		},
+
+		on: function(callback) {
+			var mod = this,
+				$ctx = mod.$ctx;
+
+
+
+			callback();
+		},
+
+		after: function() {
+			var mod = this,
+				$ctx = mod.$ctx;
+
+
+		}
+
+	});
+}(Tc.$));
