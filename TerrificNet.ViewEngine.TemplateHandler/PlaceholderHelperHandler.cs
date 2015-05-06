ffb77223ebdd38bf3093d9ee81f242b13d47fc74
Stefan Schälle <stefan.schaelle@namics.com>
2015-05-06 16:33:21 +0200

    Placeholder with dynamic index added

--- TerrificNet.ViewEngine.TemplateHandler/PlaceholderHelperHandler.cs
+++ TerrificNet.ViewEngine.TemplateHandler/PlaceholderHelperHandler.cs
@@ -1,33 +1,36 @@
 using System;
 using System.Collections.Generic;
+using System.Reflection;
 using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json.Schema;
 using Veil;
 using Veil.Helper;
 
 namespace TerrificNet.ViewEngine.TemplateHandler
 {
-    internal class PlaceholderHelperHandler : IHelperHandler 
-    {
-        private readonly ITerrificTemplateHandler _handler;
+	internal class PlaceholderHelperHandler : IHelperHandler
+	{
+		private readonly ITerrificTemplateHandler _handler;
 
-        public PlaceholderHelperHandler(ITerrificTemplateHandler handler)
-        {
-            _handler = handler;
-        }
+		public PlaceholderHelperHandler(ITerrificTemplateHandler handler)
+		{
+			_handler = handler;
+		}
 
-        public bool IsSupported(string name)
-        {
-            return name.StartsWith("placeholder", StringComparison.OrdinalIgnoreCase);
-        }
+		public bool IsSupported(string name)
+		{
+			return name.StartsWith("placeholder", StringComparison.OrdinalIgnoreCase);
+		}
 
-        public Task EvaluateAsync(object model, RenderingContext context, IDictionary<string, string> parameters)
-        {
-            var key = parameters["key"].Trim('"');
-	        var index = TryGetIndex(parameters, model);
-	        return _handler.RenderPlaceholderAsync(model, key, index, context);
-        }
+		public Task EvaluateAsync(object model, RenderingContext context, IDictionary<string, string> parameters)
+		{
+			var key = parameters["key"].Trim('"');
+			var index = TryGetIndex(parameters, model);
+			return _handler.RenderPlaceholderAsync(model, key, index, context);
+		}
 
-	    public void Evaluate(object model, RenderingContext context, IDictionary<string, string> parameters)
+		public void Evaluate(object model, RenderingContext context, IDictionary<string, string> parameters)
 		{
 			var key = parameters["key"].Trim('"');
 			var index = TryGetIndex(parameters, model);
@@ -37,19 +40,50 @@ namespace TerrificNet.ViewEngine.TemplateHandler
 		private static int? TryGetIndex(IDictionary<string, string> parameters, object model)
 		{
 			string indexProperty;
-			if (!parameters.TryGetValue("index", out indexProperty)) 
+			if (!parameters.TryGetValue("index", out indexProperty))
 				return null;
 
+			indexProperty = indexProperty.Trim('"');
+
 			int? index = null;
-			var indexLocal = GetPropValue(model, indexProperty) as int?;
-			if (indexLocal != null)
+			int indexLocal;
+			if (TryGetPropValue(model, indexProperty, out indexLocal))
 				index = indexLocal;
+
 			return index;
 		}
 
-		private static object GetPropValue(object src, string propName)
+		private static bool TryGetPropValue<TValue>(object src, string propertyName, out TValue value)
 		{
-			return src.GetType().GetProperty(propName).GetValue(src, null);
+			value = default(TValue);
+
+			//JObject
+			var jObject = src as JObject;
+			JToken jValue;
+			if (jObject != null && jObject.TryGetValue(propertyName, StringComparison.InvariantCultureIgnoreCase, out jValue))
+			{
+				value = jValue.Value<TValue>();
+				return true;
+			}
+
+			//Dictionary
+			var dictionaryObject = src as IDictionary<string, object>;
+			object dictionaryValue;
+			if (dictionaryObject != null && dictionaryObject.TryGetValue(propertyName, out dictionaryValue) && dictionaryValue is TValue)
+			{
+				value = (TValue)dictionaryValue;
+				return true;
+			}
+
+			var property = src.GetType().GetProperty(propertyName, BindingFlags.IgnoreCase);
+			if (property != null)
+			{
+				value = (TValue)property.GetValue(src, null);
+
+				return true;
+			}
+
+			return false;
 		}
-    }
+	}
 }
