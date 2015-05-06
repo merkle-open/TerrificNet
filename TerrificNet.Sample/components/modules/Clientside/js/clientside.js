71170f9e17d1233b40daa20fce019bab1d9d28a0
Stefan Schälle <schaelle@gmail.com>
2015-05-06 23:13:43 +0200

    Clientside test example

---
+++ TerrificNet.Sample/components/modules/Clientside/js/clientside.js
@@ -0,0 +1,53 @@
+(function($) {
+	"use strict";
+	/**
+	 * Carousel module implementation.
+	 *
+	 * @namespace Tc.Module
+	 * @class Carousel
+	 * @extends Tc.Module
+	 */
+	Tc.Module.Clientside = Tc.Module.extend({
+
+		dataTemplate: null,
+
+		init: function($ctx, sandbox, modId) {
+			this._super($ctx, sandbox, modId);
+
+			this.dataTemplate = $ctx.data('template');
+		},
+
+		bind: function() {
+			var mod = this,
+				$ctx = mod.$ctx;
+			
+			$ctx.on('click', function(){
+				console.log('click');
+				
+				var renderData = {
+					'title': 'Clientside ' + new Date(),
+					'title2':'additional client side data'
+				};
+				Tcn.ViewEngine.loadAndRenderAsync(mod.dataTemplate, renderData).then(function (data) {
+					$ctx.html(data);
+					mod.bind();
+				});
+
+			});
+		},
+
+		on: function(callback) {
+			var mod = this;
+
+			mod.bind();
+
+			callback();
+		},
+
+		after: function() {
+			var mod = this,
+				$ctx = mod.$ctx;
+		}
+
+	});
+}(Tc.$));
